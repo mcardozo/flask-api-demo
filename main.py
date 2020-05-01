be6e9ab3126f8730b7d4a9c35c90ce6daa8e1aa4
Marisol Cardozo <cardozo.marisolp@gmail.com>
2020-05-01 15:00:50 -0300

    Agrega endpoint con lista de casos confirmados con covid-19 en argentina.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from flask import Flask
 from flask import jsonify
 
@@ -6,3 +9,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({'status': 200})
+
+@app.route('/confirmed-cases')
+def get_list_confirmed_cases():
+    filename = os.path.join(app.root_path, 'data', 'confirmed-cases.json')
+    with open(filename) as test_file:
+        return json.load(test_file)
